42a80b9bbc049d44ef575cfef903bcde13004399
Luke-Sturm <lukejared2004@gmail.com>
2026-02-13 17:46:40 -0600

    Initial Commit

---
+++ Sturm_Note_Taking_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sturm_Note_Taking_App
+//
+//  Created by Luke Sturm on 2/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sturm_Note_Taking_App/Sturm_Note_Taking_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sturm_Note_Taking_AppApp.swift
+//  Sturm_Note_Taking_App
+//
+//  Created by Luke Sturm on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Sturm_Note_Taking_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
